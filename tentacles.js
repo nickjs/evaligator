7e059417df7b5acee718dd1680b8b0bc847dfd12
Nick Small <nick@shopify.com>
2012-01-24 01:45:04 -0500

    merge m's stuff

--- tentacles.js
+++ tentacles.js
@@ -1,24 +1,103 @@
 self.onmessage = function(msg) {
-  try {
-    var lines = msg.data.split('\n');
-    var vars = [], code = '', display = '';
+  var cesspoolOfVariables = {}; // all the introduced variables and their current and previous values
+  var result = '';
 
-    var varRegex = /\s*var\s*(\w+|\d*)/;
+  var allTheCode = msg.data;
+  if (allTheCode){
+    var allTheLines = allTheCode.split("\n")
 
-    var i = 0, count = lines.length, line, matches;
-    for(; i < count; i++) {
-      line = lines[i];
-      code += line + ';'
+    for (i = 0; i < allTheLines.length; ++i){
 
-      matches = line.match(varRegex);
-      if (matches.length > 1) {
-        vars.push(matches[1]);
-        display += matches[1] + " = " + (new Function(line + ';return ' + matches[1])());
+      if ( isAVariableDefinition(allTheLines[i]) ){
+        // ooey, new variable. let's add it to the map and print it out
+        // TODO: what if we redefine a variable?
+        var newVar = allTheLines[i].replace(/var/g, " ").split("="); // this line is reliable as fuck
+        cesspoolOfVariables[newVar[0].trim()] = {name: newVar[0].trim(), previous: -1, current: newVar[1]}
+
+        result += newVar[0] + " = " + newVar[1];
+      }
+      else if ( isSomeTypeOfValueChange(allTheLines[i]) ){
+        // re-eval the var and reassign it to the map
+        var thisVar = cesspoolOfVariables[whichVariableIsBeingChanged(allTheLines[i])];
+
+        if ( thisVar ){
+          // first thing: to eval, we need to introduce the variable in the context with the prev. value
+          // second thing: if this was added as a tentacle, then the value of this var isn't a number
+          // it's the name of a different var. so we need to trace back to the value of that var
+          var fakeInitializeString = thisVar.name + "=" + thisVar.current + ";";
+
+          if ( isVarATentacle(thisVar.current))
+          {
+            // add the value of the tentacle to the context
+            fakeInitializeString = thisVar.current + "=" + findTentacleValue(thisVar.current)
+                      + "; " + fakeInitializeString;
+          }
+
+          var amazingEvalString = fakeInitializeString + allTheLines[i];
+          eval(amazingEvalString);
+          var amazingEvalResult = eval(thisVar.name);
+
+          // reassign it to the map
+          thisVar.previous = thisVar.current;
+          thisVar.current = amazingEvalResult;
+          cesspoolOfVariables[thisVar.name] = thisVar;
+
+          result += thisVar.name + " = " + thisVar.current;
+          // TODO: if the original value was a tentacle, should we display ALL of the tentacles?
+
+        }
       }
 
-      display += '\n'
+      result += '\n';
     }
+  }
 
-    self.postMessage(display);
-  } catch (e) {}
+  self.postMessage(result);
+
+  function isAVariableDefinition(codeLine){
+    return (codeLine.indexOf("var") != -1);
+  }
+  function isSomeTypeOfValueChange(codeLine){
+    return (codeLine.indexOf("+=") != -1 ||
+        codeLine.indexOf("-=") != -1 ||
+        codeLine.indexOf("++") != -1 ||
+        codeLine.indexOf("--") != -1 ||
+        codeLine.indexOf("*=") != -1);
+  }
+  function whichVariableIsBeingChanged(codeLine){
+    // hurrah for hideous code!
+    if (codeLine.indexOf("+=") != -1)
+      return codeLine.split("+=")[0].trim();
+    if (codeLine.indexOf("-=") != -1)
+      return codeLine.split("-=")[0].trim();
+    if (codeLine.indexOf("++") != -1)
+      return codeLine.split("++")[0].trim();
+    if (codeLine.indexOf("--") != -1)
+      return codeLine.split("--")[0].trim();
+    if (codeLine.indexOf("*=") != -1)
+      return codeLine.split("*=")[0].trim();
+    return "";
+  }
+
+  // dealing with tentacles
+  function isVarATentacle(varName){
+    try{
+      return cesspoolOfVariables[varName.trim()] != null
+    }
+    catch(err)
+    {
+      return false;
+    }
+  }
+
+  function findTentacleValue(varName){
+    var varObject = cesspoolOfVariables[varName.trim()];
+    if (!varObject)
+      return undefined;
+
+    if (!isVarATentacle(varObject.current))
+      return varObject.current;
+    else
+      return findTentacleValue(varObject.current);  // hey there recursion!
+  }
 }
