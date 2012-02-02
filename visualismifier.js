736ed49aa8970af77705eb3d39bcdb3eba1417b8
Nick Small <nick@shopify.com>
2012-02-01 21:28:38 -0500

    cleanup

--- visualismifier.js
+++ visualismifier.js
@@ -5,7 +5,6 @@
   var Split = require('ace/split').Split;
   var theme = require('ace/theme-twilight');
 
-  var worker = new Worker('tentacles.js');
   var split = new Split(document.getElementById('editor'), theme, 2);
   split.getEditor(1).setReadOnly(true);
 
@@ -13,28 +12,31 @@
   editor.focus();
   // editor.getSession().setMode(Mode)
 
-  editor.getSession().on('change', function(e) {
-    var text = editor.getSession().getDocument().getValue();
-    // worker.postMessage(text);
-    var tree = Parser.Parser.parse(text);
-    var result = {source: ''}
-    walkTheTreeBitches(tree, result);
-
-    console.log(result);
-    split.getEditor(1).getSession().setValue(result.source);
-  });
-
-  worker.onmessage = function(msg) {
-    split.getEditor(1).getSession().setValue(msg.data);
-  }
-
   window.onresize = function() {
     split.resize();
   }
 
+  editor.getSession().on('change', function(e) {
+    var text = editor.getSession().getDocument().getValue();
+    var tree = Parser.Parser.parse(text);
+    var result = {source: ''};
+
+    walkTheTreeBitches(tree, result);
+
+    split.getEditor(1).getSession().setValue(result.source);
+  });
+
   var nicksPoorlyNamedMap = {
-    'VariableDeclaration': printAssignment,
-    'ForStatement': printLoop
+    VariableDeclaration: function(node, source)
+    {
+      var identifier = findNodesChildWithName(node, 'Identifier');
+      source.source += ' ' + identifier + ' = ' + new Function(node.source + ';return ' + identifier + ';')() + '\n';
+    },
+
+    ForStatement: function(node, source)
+    {
+
+    }
   };
 
   function walkTheTreeBitches(tree, source)
@@ -51,17 +53,6 @@
 
         walkTheTreeBitches(children[i], source);
     }
-  }
-
-  function printAssignment(node, source)
-  {
-    var identifier = findNodesChildWithName(node, 'Identifier');
-    source.source += ' ' + identifier + ' = ' + new Function(node.source + ';return ' + identifier + ';')() + '\n';
-  }
-
-  function printLoop()
-  {
-
   }
 
   function findNodesChildWithName(node, name)
